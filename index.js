a0d864abe2b237efc6404809a895801f672716c9
evamomanyi <evanyaks13@gmail.com>
2026-04-23 10:56:39 +0300

    JS Functions

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+//module.exports = {
+ // ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+//};
+
+function createLoginTracker(userInfo) {
+  userInfo.username = "user1"
+  userInfo.password = "password123"
+  
+    let attemptCount = 0
+      return (passwordAttempt) => {
+         attemptCount++;
+       
+      
+        if (attemptCount > 3) {
+          return "Your account is locked due to many failed attempts";
+        } else if (passwordAttempt === userInfo.password) {
+          return "Your login was successful";
+        } else {
+          return `Incorrect password. ${3-attemptCount} attempt(s) remaining`;
+        } 
+      };
+     
+}
 
 
-
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
