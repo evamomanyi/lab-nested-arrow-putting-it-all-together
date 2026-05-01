52c455be8bb19e1eb86e4df9e5a127cea89bfc1a
evamomanyi <evanyaks13@gmail.com>
2026-05-01 10:01:54 +0300

    corrections on file

--- index.js
+++ index.js
@@ -9,17 +9,26 @@ function createLoginTracker(userInfo) {
     let attemptCount = 0
       return (passwordAttempt) => {
          attemptCount++;
+         
+         if (passwordAttempt === userInfo.password) {
+          return "Login successful";
+         } else if (passwordAttempt !== userInfo.password && attemptCount <= 3) {
+          return `Attempt ${attemptCount}: Login failed`;
+         }else if (passwordAttempt !== userInfo.password && attemptCount > 3) {
+          return "Account locked due to too many failed login attempts";
+         }
+        }
+      }
+
        
       
-        if (attemptCount > 3) {
-          return "Your account is locked due to many failed attempts";
-        } else if (passwordAttempt === userInfo.password) {
-          return "Your login was successful";
-        } else {
-          return `Incorrect password. ${3-attemptCount} attempt(s) remaining`;
-        } 
-      };
+//         //if (attemptCount > 3) {
+//           return "Your account is locked due to many failed attempts";
+//         } else if (passwordAttempt === userInfo.password) {
+//           return "Your login was successful";
+//         } else {
+//           return `Incorrect password. ${3-attemptCount} attempt(s) remaining`;
+//         } 
+//       };
      
-}
-
-
+// 
